a8d097ddf7cb6f493be6c2a7fc616c0ba4e64ecd
Sanjana Paye <paye@usc.edu>
2024-09-03 16:46:39 -0400

    updated comments

--- codetorunfigures.R
+++ codetorunfigures.R
@@ -5,14 +5,14 @@ ncptraversals(0.3, 0.1, 1, 1)
 haploidpenetrancefigure(0.3, 0.1, c(0.2, 0.5, 1))
 
 #figure3
-diploidpenetrancefigure(0.3, 0.1, c(0.2, 0.5, 1))
-
-#riskvsprotectivefigure(0.3, 0.1, 1)
-
 dominancefigure(0.3, 0.1, c(0.1, 0.5, 0.9))
+dominancefigure.simulation(0.3, 0.1, c(0.1, 0.5, 0.9))
 
+#figure 4
 ncppowersimfigure(0.3, 0.1)
 
+#figure 5
 armitagetrendfigure(0.3, 0.1)
 
+#figure 6
 logisticregressionfigure(0.3, 0.1)
